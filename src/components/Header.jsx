8d2a4a682472be60e722c8b70383c5eed9f86c77
sham2507 <117130012+sham2507@users.noreply.github.com>
2024-03-22 15:15:08 +0530

    made changes in the info section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header(){
             <img  src={game_logo}/>
             <h1>Investment Calculator <span onClick = {handleMouseOver} className="showInfo">&#9432;</span></h1>            
             <article>                
-                {showInfo ? <div className="info"><span className="content"></span></div> : null}
+                {showInfo ? <div className="info"><span className="content">Hello Folks, this is a investment calculator application that accepts several inputs and serves you a set of information like interest value alone at the end of every year, and total captial amount that you have invested without interest amount based on your expected returns.</span></div> : null}
             </article>
         </header>
     );
